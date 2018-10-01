8414ef137a0f90a3af19d29de20aeeffe19b159e
lukkadd <lukkad@yahoo.com.br>
2018-10-01 08:41:39 -0300

    Inserted documentation for stack.c/.h

--- stack.h
+++ stack.h
@@ -1,10 +1,33 @@
+/**
+	Simple stack struct definition
+	Contains a float value and a pointer to the next
+	stack node
+**/
 typedef struct Stack{
 	float value;
 	struct Stack *next;
 }Stack;
 
+/** FUNCTION: push
+ * INPUT: float, Stack**
+ * OUTPUT: void
+ * DESCRIPTION: pushes a float value to the top
+ * of the stack given
+ **/
 void push(float v, Stack** head);
 
+/** FUNCTION: isempty
+ * INPUT: Stack*
+ * OUTPUT: int
+ * DESCRIPTION: if the given stack is empty returns 1
+ * otherwise it returns 0
+ **/
 int isempty(Stack* head);
 
+/** FUNCTION: pop
+ * INPUT: Stack **
+ * OUTPUT: float
+ * DESCRIPTION: returns the float value located
+ * on the top of the stack, removing it
+ **/
 float pop(Stack** head);
